ac35589c005d8af2982344431801ac7e713ce67a
25200246 <25200246@tokeninc.com>
2023-05-26 17:08:00 +0300

    ##TR400RET-1805##PAYMENT##Added TransactionDB with Room for Sale. First, added dependencies. Created repository, dao and instance file for that. For now, created database APP_TEMP2 for check database is working truly or not.##

---
+++ app/src/main/java/com/example/application_template_jmvvm/Helpers/DataBase/entities/TransactionEntity.java
@@ -0,0 +1,410 @@
+package com.example.application_template_jmvvm.Helpers.DataBase.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.example.application_template_jmvvm.Helpers.DataBase.DatabaseInfo;
+
+@Entity(tableName = DatabaseInfo.TRANSACTIONTABLE)
+public class TransactionEntity {
+    @ColumnInfo(name = "col_uuid")
+    public String uuid;
+
+    @ColumnInfo(name = "col_ulSTN")
+    public String ulSTN;
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "col_ulGUP_SN")
+    public int ulGUP_SN;
+
+    @ColumnInfo(name = "col_batchNo")
+    public int batchNo;
+
+    @ColumnInfo(name = "col_settleNo")
+    public int settleNo;
+
+    @ColumnInfo(name = "col_bCardReadType")
+    public int bCardReadType;
+
+    @ColumnInfo(name = "col_bTransCode")
+    public int bTransCode;
+
+    @ColumnInfo(name = "col_ulAmount")
+    public int ulAmount;
+
+    @ColumnInfo(name = "col_ulAmount2")
+    public int ulAmount2;
+
+    @ColumnInfo(name = "col_baPAN")
+    public String baPAN;
+
+    @ColumnInfo(name = "col_baExpDate")
+    public String baExpDate;
+
+    @ColumnInfo(name = "col_baDate")
+    public String baDate;
+
+    @ColumnInfo(name = "col_baTime")
+    public String baTime;
+
+    @ColumnInfo(name = "col_baTrack2")
+    public String baTrack2;
+
+    @ColumnInfo(name = "col_baCustomName")
+    public String baCustomName;
+
+    @ColumnInfo(name = "col_baRspCode")
+    public String baRspCode;
+
+    @ColumnInfo(name = "col_isVoid")
+    public int isVoid;
+
+    @ColumnInfo(name = "col_bInstCnt")
+    public int bInstCnt;
+
+    @ColumnInfo(name = "col_ulInstAmount")
+    public int ulInstAmount;
+
+    @ColumnInfo(name = "col_baTranDate")
+    public String baTranDate;
+
+    @ColumnInfo(name = "col_baTranDate2")
+    public String baTranDate2;
+
+    @ColumnInfo(name = "col_baHostLogKey")
+    public String baHostLogKey;
+
+    @ColumnInfo(name = "col_stChipData")
+    public String stChipData;
+
+    @ColumnInfo(name = "col_isSignature")
+    public int isSignature;
+
+    @ColumnInfo(name = "col_stPrintData1")
+    public String stPrintData1;
+
+    @ColumnInfo(name = "col_stPrintData2")
+    public String stPrintData2;
+
+    @ColumnInfo(name = "col_baVoidDateTime")
+    public String baVoidDateTime;
+
+    @ColumnInfo(name = "col_authCode")
+    public String authCode;
+
+    @ColumnInfo(name = "col_aid")
+    public String aid;
+
+    @ColumnInfo(name = "col_aidLabel")
+    public String aidLabel;
+
+    @ColumnInfo(name = "col_pinByPass")
+    public int pinByPass;
+
+    @ColumnInfo(name = "col_displayData")
+    public String displayData;
+
+    @ColumnInfo(name = "col_baCVM")
+    public String baCVM;
+
+    @ColumnInfo(name = "col_isOffline")
+    public int isOffline;
+
+    @ColumnInfo(name = "col_SID")
+    public String SID;
+
+    @ColumnInfo(name = "col_is_onlinePIN")
+    public int isOnlinePIN;
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setUlSTN(String ulSTN) {
+        this.ulSTN = ulSTN;
+    }
+
+    public void setUlGUP_SN(int ulGUP_SN) {
+        this.ulGUP_SN = ulGUP_SN;
+    }
+
+    public void setBatchNo(int batchNo) {
+        this.batchNo = batchNo;
+    }
+
+    public void setSettleNo(int settleNo) {
+        this.settleNo = settleNo;
+    }
+
+    public void setbCardReadType(int bCardReadType) {
+        this.bCardReadType = bCardReadType;
+    }
+
+    public void setbTransCode(int bTransCode) {
+        this.bTransCode = bTransCode;
+    }
+
+    public void setUlAmount(int ulAmount) {
+        this.ulAmount = ulAmount;
+    }
+
+    public void setUlAmount2(int ulAmount2) {
+        this.ulAmount2 = ulAmount2;
+    }
+
+    public void setBaPAN(String baPAN) {
+        this.baPAN = baPAN;
+    }
+
+    public void setBaExpDate(String baExpDate) {
+        this.baExpDate = baExpDate;
+    }
+
+    public void setBaDate(String baDate) {
+        this.baDate = baDate;
+    }
+
+    public void setBaTime(String baTime) {
+        this.baTime = baTime;
+    }
+
+    public void setBaTrack2(String baTrack2) {
+        this.baTrack2 = baTrack2;
+    }
+
+    public void setBaCustomName(String baCustomName) {
+        this.baCustomName = baCustomName;
+    }
+
+    public void setBaRspCode(String baRspCode) {
+        this.baRspCode = baRspCode;
+    }
+
+    public void setIsVoid(int isVoid) {
+        this.isVoid = isVoid;
+    }
+
+    public void setbInstCnt(int bInstCnt) {
+        this.bInstCnt = bInstCnt;
+    }
+
+    public void setUlInstAmount(int ulInstAmount) {
+        this.ulInstAmount = ulInstAmount;
+    }
+
+    public void setBaTranDate(String baTranDate) {
+        this.baTranDate = baTranDate;
+    }
+
+    public void setBaTranDate2(String baTranDate2) {
+        this.baTranDate2 = baTranDate2;
+    }
+
+    public void setBaHostLogKey(String baHostLogKey) {
+        this.baHostLogKey = baHostLogKey;
+    }
+
+    public void setStChipData(String stChipData) {
+        this.stChipData = stChipData;
+    }
+
+    public void setIsSignature(int isSignature) {
+        this.isSignature = isSignature;
+    }
+
+    public void setStPrintData1(String stPrintData1) {
+        this.stPrintData1 = stPrintData1;
+    }
+
+    public void setStPrintData2(String stPrintData2) {
+        this.stPrintData2 = stPrintData2;
+    }
+
+    public void setBaVoidDateTime(String baVoidDateTime) {
+        this.baVoidDateTime = baVoidDateTime;
+    }
+
+    public void setAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
+
+    public void setAid(String aid) {
+        this.aid = aid;
+    }
+
+    public void setAidLabel(String aidLabel) {
+        this.aidLabel = aidLabel;
+    }
+
+    public void setPinByPass(int pinByPass) {
+        this.pinByPass = pinByPass;
+    }
+
+    public void setDisplayData(String displayData) {
+        this.displayData = displayData;
+    }
+
+    public void setBaCVM(String baCVM) {
+        this.baCVM = baCVM;
+    }
+
+    public void setIsOffline(int isOffline) {
+        this.isOffline = isOffline;
+    }
+
+    public void setSID(String SID) {
+        this.SID = SID;
+    }
+
+    public void setIsOnlinePIN(int isOnlinePIN) {
+        this.isOnlinePIN = isOnlinePIN;
+    }
+
+
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getUlSTN() {
+        return ulSTN;
+    }
+
+    public int getUlGUP_SN() {
+        return ulGUP_SN;
+    }
+
+    public int getBatchNo() {
+        return batchNo;
+    }
+
+    public int getSettleNo() {
+        return settleNo;
+    }
+
+    public int getbCardReadType() {
+        return bCardReadType;
+    }
+
+    public int getbTransCode() {
+        return bTransCode;
+    }
+
+    public int getUlAmount() {
+        return ulAmount;
+    }
+
+    public int getUlAmount2() {
+        return ulAmount2;
+    }
+
+    public String getBaPAN() {
+        return baPAN;
+    }
+
+    public String getBaExpDate() {
+        return baExpDate;
+    }
+
+    public String getBaDate() {
+        return baDate;
+    }
+
+    public String getBaTime() {
+        return baTime;
+    }
+
+    public String getBaTrack2() {
+        return baTrack2;
+    }
+
+    public String getBaCustomName() {
+        return baCustomName;
+    }
+
+    public String getBaRspCode() {
+        return baRspCode;
+    }
+
+    public int getIsVoid() {
+        return isVoid;
+    }
+
+    public int getbInstCnt() {
+        return bInstCnt;
+    }
+
+    public int getUlInstAmount() {
+        return ulInstAmount;
+    }
+
+    public String getBaTranDate() {
+        return baTranDate;
+    }
+
+    public String getBaTranDate2() {
+        return baTranDate2;
+    }
+
+    public String getBaHostLogKey() {
+        return baHostLogKey;
+    }
+
+    public String getStChipData() {
+        return stChipData;
+    }
+
+    public int getIsSignature() {
+        return isSignature;
+    }
+
+    public String getStPrintData1() {
+        return stPrintData1;
+    }
+
+    public String getStPrintData2() {
+        return stPrintData2;
+    }
+
+    public String getBaVoidDateTime() {
+        return baVoidDateTime;
+    }
+
+    public String getAuthCode() {
+        return authCode;
+    }
+
+    public String getAid() {
+        return aid;
+    }
+
+    public String getAidLabel() {
+        return aidLabel;
+    }
+
+    public int getPinByPass() {
+        return pinByPass;
+    }
+
+    public String getDisplayData() {
+        return displayData;
+    }
+
+    public String getBaCVM() {
+        return baCVM;
+    }
+
+    public int getIsOffline() {
+        return isOffline;
+    }
+
+    public String getSID() {
+        return SID;
+    }
+
+    public int getIsOnlinePIN() {
+        return isOnlinePIN;
+    }
+
+}
--- app/src/main/java/com/example/application_template_jmvvm/Services/TransactionService.java
+++ app/src/main/java/com/example/application_template_jmvvm/Services/TransactionService.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.util.Log;
 
 import com.example.application_template_jmvvm.Entity.ResponseCode;
+import com.example.application_template_jmvvm.Helpers.DataBase.entities.TransactionEntity;
 import com.example.application_template_jmvvm.Helpers.DataBase.transaction.TransactionCol;
 import com.example.application_template_jmvvm.Helpers.DataBase.transaction.TransactionDB;
 import com.example.application_template_jmvvm.Responses.OnlineTransactionResponse;
 import com.example.application_template_jmvvm.Responses.TransactionResponse;
 import com.example.application_template_jmvvm.Uicomponents.MainActivity;
+import com.example.application_template_jmvvm.Viewmodels.SaleViewModel;
 import com.token.uicomponents.infodialog.InfoDialog;
 import com.token.uicomponents.infodialog.InfoDialogListener;
 
@@ -35,7 +37,7 @@ public class TransactionService implements InfoDialogListener {
     private Observable<ContentValues> observable;
     private Observer<ContentValues> observer;
     private TransactionResponse transactionResponse;
-    public void doInBackground(MainActivity main, Context context, ContentValues values, TransactionResponseListener responseTransactionResponseListener) {
+    public void doInBackground(MainActivity main, Context context, ContentValues values, SaleViewModel saleViewModel, TransactionResponseListener responseTransactionResponseListener) {
 
         dialog = main.showInfoDialog(InfoDialog.InfoType.Progress, "Progress",false);
         observable = Observable.just(values)
@@ -70,7 +72,7 @@ public class TransactionService implements InfoDialogListener {
             public void onComplete() {
                 Log.i("Complete","Complete");
                 OnlineTransactionResponse onlineTransactionResponse = parseResponse(1);
-                TransactionResponse transactionResponse = finishTransaction(context,values,onlineTransactionResponse);
+                TransactionResponse transactionResponse = finishTransaction(context,values,onlineTransactionResponse,saleViewModel);
                 responseTransactionResponseListener.onComplete(transactionResponse);
                 try {
                     Thread.sleep(2000);
@@ -97,20 +99,62 @@ public class TransactionService implements InfoDialogListener {
         return onlineTransactionResponse;
     }
 
-    private TransactionResponse finishTransaction(Context context, ContentValues values, OnlineTransactionResponse onlineTransactionResponse){
-        values.put(TransactionCol.col_baRspCode.name(), onlineTransactionResponse.getmResponseCode().toString());
-        values.put(TransactionCol.col_stPrintData1.name(), onlineTransactionResponse.getmTextPrintCode1());
-        values.put(TransactionCol.col_stPrintData2.name(), onlineTransactionResponse.getmTextPrintCode2());
-        values.put(TransactionCol.col_authCode.name(), onlineTransactionResponse.getmAuthCode());
-        values.put(TransactionCol.col_baHostLogKey.name(), onlineTransactionResponse.getmHostLogKey());
-        values.put(TransactionCol.col_displayData.name(), onlineTransactionResponse.getmDisplayData());
-        values.put(TransactionCol.col_bInstCnt.name(), onlineTransactionResponse.getInsCount());
-        values.put(TransactionCol.col_ulInstAmount.name(), onlineTransactionResponse.getInstAmount());
-        values.put(TransactionCol.col_baVoidDateTime.name(), onlineTransactionResponse.getDateTime());
-        TransactionDB.getInstance(context).insertTransaction(values);
-        return new TransactionResponse(onlineTransactionResponse,1,values);
+    private TransactionResponse finishTransaction(Context context, ContentValues values, OnlineTransactionResponse onlineTransactionResponse, SaleViewModel saleViewModel){
+        ContentValues values1 = values;
+        values1.put(TransactionCol.col_baRspCode.name(), onlineTransactionResponse.getmResponseCode().toString());
+        values1.put(TransactionCol.col_stPrintData1.name(), onlineTransactionResponse.getmTextPrintCode1());
+        values1.put(TransactionCol.col_stPrintData2.name(), onlineTransactionResponse.getmTextPrintCode2());
+        values1.put(TransactionCol.col_authCode.name(), onlineTransactionResponse.getmAuthCode());
+        values1.put(TransactionCol.col_baHostLogKey.name(), onlineTransactionResponse.getmHostLogKey());
+        values1.put(TransactionCol.col_displayData.name(), onlineTransactionResponse.getmDisplayData());
+        values1.put(TransactionCol.col_bInstCnt.name(), onlineTransactionResponse.getInsCount());
+        values1.put(TransactionCol.col_ulInstAmount.name(), onlineTransactionResponse.getInstAmount());
+        values1.put(TransactionCol.col_baVoidDateTime.name(), onlineTransactionResponse.getDateTime());
+        TransactionEntity transactionEntity = entityCreator(values1);
+        saleViewModel.insertTransaction(transactionEntity); //TODO düzenlenecek
+        TransactionDB.getInstance(context).insertTransaction(values1);
+        return new TransactionResponse(onlineTransactionResponse,1,values1);
     }
 
+    private TransactionEntity entityCreator(ContentValues values){
+        TransactionEntity transactionEntity = new TransactionEntity();
+        transactionEntity.setUuid(values.get(TransactionCol.col_uuid.name()).toString());
+        transactionEntity.setUlSTN(values.get(TransactionCol.col_ulSTN.name()).toString());
+        //transactionEntity.setBatchNo(Integer.parseInt(values.get(TransactionCol.col_batchNo.name()).toString()));
+        //transactionEntity.setSettleNo(Integer.parseInt(values.get(TransactionCol.col_settleNo.name()).toString()));
+        transactionEntity.setbCardReadType(Integer.parseInt(values.get(TransactionCol.col_bCardReadType.name()).toString()));
+        transactionEntity.setbTransCode(Integer.parseInt(values.get(TransactionCol.col_bTransCode.name()).toString()));
+        transactionEntity.setUlAmount(Integer.parseInt(values.get(TransactionCol.col_ulAmount.name()).toString()));
+        transactionEntity.setUlAmount2(Integer.parseInt(values.get(TransactionCol.col_ulAmount2.name()).toString()));
+        transactionEntity.setBaPAN(values.get(TransactionCol.col_baPAN.name()).toString());
+        transactionEntity.setBaExpDate(values.get(TransactionCol.col_baExpDate.name()).toString());
+        transactionEntity.setBaDate(values.get(TransactionCol.col_baDate.name()).toString());
+        transactionEntity.setBaTime(values.get(TransactionCol.col_baTime.name()).toString());
+        transactionEntity.setBaTrack2(values.get(TransactionCol.col_baTrack2.name()).toString());
+        //transactionEntity.setBaCustomName(values.get(TransactionCol.col_baCustomName.name()).toString());
+        transactionEntity.setBaRspCode(values.get(TransactionCol.col_baRspCode.name()).toString());
+        transactionEntity.setIsVoid(0);     //TODO: rspcode incelenecek
+        transactionEntity.setbInstCnt(Integer.parseInt(values.get(TransactionCol.col_bInstCnt.name()).toString()));
+        transactionEntity.setUlInstAmount(Integer.parseInt(values.get(TransactionCol.col_ulInstAmount.name()).toString()));
+        transactionEntity.setBaTranDate(values.get(TransactionCol.col_baTranDate.name()).toString());
+        transactionEntity.setBaTranDate2(values.get(TransactionCol.col_baTranDate2.name()).toString());
+        transactionEntity.setBaHostLogKey(values.get(TransactionCol.col_baHostLogKey.name()).toString());
+        //transactionEntity.setStChipData(values.get(TransactionCol.col_stChipData.name()).toString());
+        transactionEntity.setIsSignature(0);
+        transactionEntity.setStPrintData1(values.get(TransactionCol.col_stPrintData1.name()).toString());
+        transactionEntity.setStPrintData2(values.get(TransactionCol.col_stPrintData2.name()).toString());
+        //transactionEntity.setBaVoidDateTime(values.get(TransactionCol.col_baVoidDateTime.name()).toString());
+        transactionEntity.setAuthCode(values.get(TransactionCol.col_authCode.name()).toString());
+        transactionEntity.setAid(values.get(TransactionCol.col_aid.name()).toString());
+        transactionEntity.setAidLabel(values.get(TransactionCol.col_aidLabel.name()).toString());
+        transactionEntity.setPinByPass(0);
+        transactionEntity.setDisplayData(values.get(TransactionCol.col_displayData.name()).toString());
+        transactionEntity.setBaCVM(values.get(TransactionCol.col_baCVM.name()).toString());
+        transactionEntity.setIsOffline(0);
+        transactionEntity.setSID(values.get(TransactionCol.col_SID.name()).toString());
+        transactionEntity.setIsOnlinePIN(0);
+        return transactionEntity;
+    }
     @Override
     public void confirmed(int i) {
 
