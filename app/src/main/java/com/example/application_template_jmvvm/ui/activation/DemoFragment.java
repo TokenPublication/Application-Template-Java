cc8ce80b5ae637c46914a030bd0a708d547ab72d
25200246 <25200246@tokeninc.com>
2023-08-17 13:52:21 +0300

    ##TR400RET-1931##PAYMENT##Deleted enable and disable buttons. Replaced with switch for demo mode status.

--- app/src/main/java/com/example/application_template_jmvvm/ui/activation/DemoFragment.java
+++ app/src/main/java/com/example/application_template_jmvvm/ui/activation/DemoFragment.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.application_template_jmvvm.MainActivity;
 import com.example.application_template_jmvvm.R;
@@ -35,23 +35,11 @@ public class DemoFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         SharedPreferences sharedPreferences = mainActivity.getSharedPreferences("myprefs", Context.MODE_PRIVATE);
         boolean isEnabled = sharedPreferences.getBoolean("demo_mode", false);
-        TextView demoStatus = view.findViewById(R.id.demoStatusTextView);
-        if (isEnabled) {
-            demoStatus.setText(mainActivity.getString(R.string.demo_mode_enabled));
-        } else {
-            demoStatus.setText(mainActivity.getString(R.string.demo_mode_disabled));
-        }
-        view.findViewById(R.id.enableButton).setOnClickListener(v -> {
-            changeMode(sharedPreferences, true);
-            demoStatus.setText(mainActivity.getString(R.string.demo_mode_enabled));
-        });
-        view.findViewById(R.id.disableButton).setOnClickListener(v -> {
-            changeMode(sharedPreferences, false);
-            demoStatus.setText(mainActivity.getString(R.string.demo_mode_disabled));
-        });
+        SwitchCompat demoStatus = view.findViewById(R.id.demoSwitch);
+        demoStatus.setChecked(isEnabled);
+        view.findViewById(R.id.demoSwitch).setOnClickListener(v -> changeMode(sharedPreferences, !isEnabled));
     }
 
     private void changeMode(SharedPreferences sharedPreferences, boolean isEnabled) {
